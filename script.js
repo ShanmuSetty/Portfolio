86adb8d47c95f5f598959eb521f82b97753d4568
Shanmu Setty <shanmu@Shanmus-MacBook-Air.local>
2024-05-28 19:49:16 +0530

    Responsiveness

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-const track1 = document.getElementById("image-track1");
-const imageWidth = 40;
-window.onmousedown = (e) => {
-  track1.dataset.mouseDownAt = e.clientX;
-};
-window.onmouseup = (e) => {
-  track1.dataset.mouseDownAt = "0";
-  track1.dataset.prevPercentage = track1.dataset.percentage;
-};
-window.onmousemove = (e) => {
-  if (track1.dataset.mouseDownAt === "0") return;
-  const mouseDelta = parseFloat(track1.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth / 2;
-  const percentage = (mouseDelta / maxDelta) * -100;
-  let nextPercentage = parseFloat(track1.dataset.prevPercentage) + percentage;
-  nextPercentage = Math.min(nextPercentage, 0);
-  nextPercentage = Math.max(nextPercentage, -90);
-  track1.dataset.percentage = nextPercentage;
-  track1.animate(
-    {
-      transform: `translate(${nextPercentage + 20}%, -100%)`,
-    },
-    { duration: 1200, fill: "forwards" }
-  );
 
-  for (const image of track1.getElementsByClassName("image")) {
-    image.animate(
-      {
-        objectPosition: `${nextPercentage + 100}% 50%`,
-      },
-      { duration: 1200, fill: "forwards" }
-    );
-  }
-};
 const upArrow = document.getElementById("arrow");
 upArrow.addEventListener("click", function () {
   window.scrollTo({ top: 0, behavior: "smooth" });
@@ -80,13 +47,3 @@ window.onclick = function (event) {
     drp.classList.remove("open");
   }
 };
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("alertBoxContainer").style.display = "block";
-  }, 5000);
-});
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    document.getElementById("alertBoxContainer").style.display = "none";
-  }, 9000);
-});
